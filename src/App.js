ddc60aeece2f286b2acd4afb67f1ed1c9a202444
Natalie Baker <nathelene76@gmail.com>
2023-09-18 12:02:29 -0600

    add card component

--- src/App.js
+++ src/App.js
@@ -3,8 +3,12 @@ import { Router } from 'react-router-dom';
 import './App.css';
 import Nav from './components/Nav'
 import { Routes, Route } from 'react-router-dom'
+import './ArticleMockData'
 
 function App() {
+
+// const articleCards = 
+
 
   return (
     <div className="App">
---
+++ src/components/Card.js
@@ -0,0 +1,12 @@
+import './Card.css'
+
+export default function Card() {
+    return (
+        <div className="article-card">
+            <h3>Article Title Here</h3>
+                <p>Article Description Here</p>
+                <p>Article Date Here</p>
+                <img/>
+        </div>
+    )
+}
