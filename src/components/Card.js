17d70a981d22b9dfc98b6e1fe54cc23fdfdbf639
Natalie Baker <nathelene76@gmail.com>
2023-09-18 13:24:48 -0600

    add navlink to each card

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,12 +1,17 @@
 import './Card.css'
+import { NavLink } from 'react-router-dom'
 
 export default function Card({title, description, date, img}) {
     return (
+        
         <div className="article-card">
             <h3>{title}</h3>
                 <p>{description}</p>
                 <p>{date}</p>
+                <NavLink to='/article'>
                 <img src={img}/>
+                </NavLink>
         </div>
+   
     )
 }
