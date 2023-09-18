67b57155b3c07752db160952dad775d94e1034d1
Natalie Baker <nathelene76@gmail.com>
2023-09-18 11:45:34 -0600

    import Nav to main page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 
+import { Router } from 'react-router-dom';
 import './App.css';
 import Nav from './components/Nav'
-// import { Routes, Route } from 'react-router-dom'
+import { Routes, Route } from 'react-router-dom'
 
 function App() {
 
   return (
     <div className="App">
      <Nav />
+    <Routes>
+      <Route path="/" element={ <div className="home"> Articles Here </div>}/>
+    </Routes>
     
     </div>
   );
