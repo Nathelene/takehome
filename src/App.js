9c51314a95ccc29262856213953a69a76349773d
Natalie Baker <nathelene76@gmail.com>
2023-09-18 11:33:21 -0600

    add nav component

---
+++ src/App.js
@@ -0,0 +1,16 @@
+
+import './App.css';
+import Nav from './components/Nav'
+// import { Routes, Route } from 'react-router-dom'
+
+function App() {
+
+  return (
+    <div className="App">
+     <Nav />
+    
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Nav.js
@@ -0,0 +1,11 @@
+import './Nav.css'
+
+export default function Nav() {
+
+
+    return (
+        <nav className="nav">
+            <h1>NEWS</h1>
+        </nav>
+    )
+}
